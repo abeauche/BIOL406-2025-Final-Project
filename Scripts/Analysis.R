84e3813ba48fa08409cb326c972fa20ba3d99186
Lucas Braun <lfbraun10@gmail.com>
2025-03-11 14:43:30 -0700

    Added some templates for scripts

---
+++ Scripts/Analysis.R
@@ -0,0 +1,12 @@
+# ===Analysis
+
+# First check histograms of each variable to view distribution
+
+# Scale variables (mutate(variable_scaled = scale(variable)))
+
+#Check for correlations cor(dataframe) or cor(c("varable_1_scaled","variable_2_scaled",...))
+
+#Make models and analysis of scaled variables (DO NOT SCALE RESPONSE)
+
+# Thinking a t test between trails as well as an ANCOVA of response over distance for each trail.
+
---
+++ Scripts/dataframe.R
@@ -0,0 +1,9 @@
+#This Script is for cleaning and exporting dataframe
+
+# === Import packages and dataframes
+
+#Clean out NAs
+
+#Merge with other data if needed?
+
+#Mutate dataframes needed.
